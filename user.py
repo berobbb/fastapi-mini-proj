fe0ca59f5ef5096a5df809b841d4614be89e9d22
berobbb <shashwatdimri5@gmail.com>
2026-09-08 22:32:23 +0530

    exercise done

--- user.py
+++ user.py
@@ -6,6 +6,11 @@ user_db = [(1, "john", "john@gmail.com"),(2, "jay", "jay@outlook.com"),(3, "priy
 @app.get("/")
 def users():
     return user_db
+@app.get("/users/new")
+def new_user(user_id:int,name:str,email:str):
+    user_db.append((user_id,name,email))
+    return user_db
+
 @app.get("/users/{user_id}")
 def user(user_id: int):
     for items in user_db:
@@ -13,8 +18,23 @@ def user(user_id: int):
             return {"User Details": items}
     return {"error": "User id not defined"}
 
+books = [
+{"id": 1, "title": "Python Basics", "author": "Guido"},
+{"id": 2, "title": "FastAPI Deep Dive", "author": "Tiangolo"},
+]
 
-
+@app.get("/book/{book_id}")
+def book_search(book_id:int):
+    for book in books:
+        if book["id"]==book_id:
+            return book
+@app.get("/search")
+def author_name(author:str):
+    for book in books:
+        if book["author"]==author:
+            return book
+        else:
+            return "Not Found"
 
 
 
