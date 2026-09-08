88ce11f4a6f359d82a413c1c5cb323d322027969
berobbb <shashwatdimri5@gmail.com>
2026-09-08 17:29:38 +0530

    first

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+app=FastAPI()
+@app.get("/")
+def homepage():
+    return "hey there"
+@app.get("/welcome/{organizer_name}")
+def greet(orgranizer_name:str):
+    return f"Welcome {orgranizer_name} to Event Organizer Portal"
+
+@app.get("/event-count")
+def event_count(year:int):
+    return f"Number of events planned in {year} : 5"
+if __name__=="__main__":
+    uvicorn.run(app,host="localhost",port=8000)
+
---
+++ user.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+
+app=FastAPI()
+user_db = [(1, "john", "john@gmail.com"),(2, "jay", "jay@outlook.com"),(3, "priya", "priya123@gmail.com")]
+@app.get("/")
+def users():
+    return user_db
+@app.get("/users/{user_id}")
+def user(user_id: int):
+    for items in user_db:
+        if items[0] == user_id:
+            return {"User Details": items}
+    return {"error": "User id not defined"}
+
+
+
+
+
+
+if __name__=="__user__":
+    uvicorn.run(app,host="localhost",port=8001)
